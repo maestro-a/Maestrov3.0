7310afe3e04dc47efe5a282ea0c5ca3377838992
Valak101 <51530202+Valak101@users.noreply.github.com>
2019-07-01 19:46:50 +0700

    Upload ajg

---
+++ v3.0.sh
@@ -0,0 +1,220 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+figlet Maestro
+echo ""
+echo
+echo -n $me" Masukkan Nama Anda :"
+read a
+if [ ! -z  $a ]
+then
+echo $bi"Hai "$me" $a "
+fi
+sleep 5
+echo $i"Ini adalah Username dan Password anda"
+echo $pu"Username :"$me" Maestro"
+echo $pu"Password :"$me" 100000111"
+sleep 10
+python2 Memek.pyc
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa anjing :v("   
+sleep 1
+apt update && apt upgrade -y
+apt install nano -y
+apt install git -y
+pkg install python python2 vim figlet curl -y
+clear
+sleep 1
+apt install php -y
+pip2 install mechanize -y
+pip2 install lolcat -y
+pip2 install requests -y
+clear
+sleep 1
+clear
+sleep 1
+clear
+echo
+sleep 4
+clear
+figlet Maestro
+echo $ku"["$me"⚠"$ku"]"$i"▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪"$ku"["$me"⚠"$ku"]"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Pencipta :"$me"  MAESTRO"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Facebook :"$me"  www.facebook.com/maestro10001"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Type     :"$me"  Tool Installer"
+echo $bi"["$me"+"$bi"]"$pu" 🔱Version  :"$me"  v3.0"
+echo $ku"["$me"⚠"$ku"]"$i"▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪"$ku"["$me"⚠"$ku"]"
+sleep 2
+			             
+echo " "
+echo "🔰"$cy"["$me"MENU"$cy"]"$me"🔰"
+sleep 1
+echo "⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩"
+echo $i"["$me"1"$i"]"$bi"🔀🔀|DarkFB v1.6"      
+sleep 2
+echo $i"["$me"2"$i"]"$bi"🔀🔀|DarkFB Peremuim"
+sleep 0.2
+echo $i"["$me"3"$i"]"$bi"🔀🔀|REPORT FB"	        	    
+sleep 0.3
+echo $i"["$me"4"$i"]"$bi"🔀🔀|BRUTE FORCE TAGET"
+sleep 0.4
+echo $i"["$me"5"$i"]"$bi"🔀🔀|BRUTE FORCE FB CRACKER"
+sleep 0.1
+echo $i"["$me"6"$i"]"$bi"🔀🔀|MBF"
+sleep 0.1
+echo $i"["$me"7"$i"]"$bi"🔀🔀|CLONING YAHOO"
+sleep 0.1
+echo $i"["$me"8"$i"]"$bi"🔀🔀|OSIF"
+sleep 0.1
+echo $i"["$me"9"$i"]"$bi"🔀🔀|PROFILEGUARD FB"
+sleep 0.1
+echo $i"["$me"10"$i"]"$bi"🔀 |BOT REACTION FACEBOOK"
+sleep 0.1
+echo $i"["$me"99"$i"]"$bi"🔀 |TermuxJomb"$me"(Khusus Jomblo)"
+sleep 0.1
+echo $i"["$me"999"$i"]"$bi"🔀 |Lacak IP"$me"(By: zlucifer)"
+sleep 0.1
+echo $i"["$me"99"$i"]"$bi"🔀 |TermuxJomb"$me"(Khusus Jomblo)"
+sleep 0.1
+echo $i"["$me"00"$i"]"$pu" EXIT"
+echo "⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩⏩"
+sleep 0.1
+echo 
+read -p"[########] : " pil
+			                      		
+if [ $pil = 1 ]
+then
+clear
+sleep 1
+python2 Memek1.pyc
+git clone https://github.com/mrb4n6547/Bangsad.git
+cd Bangsad $y
+python2 BgSdKw.py $y
+fi
+
+if [ $pil = 2 ]
+then
+clear
+sleep 1
+python2 Memek1.pyc
+git clone https://github.com/MaestroTool/DarkFBPremium
+cd DarkFBPremium $y
+python2 DarkFBP.pyc $y
+fi
+	     
+if [ $pil = 3 ]
+then
+clear
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report $y
+unzip Report.zip $y
+python2 Report.py $y
+fi
+
+if [ $pil = 4 ]
+then
+clear
+sleep 1
+python2 Memek1.pyc
+git clone https://github.com/MaestroTool/HackFB.git
+cd HackFB $y
+python2 hack.py $y
+fi
+
+if [ $pil = 5 ]
+then
+clear
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3 $y
+python2 crack.py $y
+fi
+
+if [ $pil = 6 ]
+then
+clear
+sleep 1
+python2 Memek1.pyc
+git clone https://github.com/MaestroTool/TolBgsD.git
+cd TolBgsD $y
+python2 MBFUCK.py $y
+fi
+
+if [ $pil = 7 ]
+then
+clear
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning $y
+pip2 install requests mechanize $y
+pip2 install requirements $y
+python2 cloning.py $y
+fi
+
+if [ $pil = 8 ]
+then
+clear
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF $y
+pip2 install -r requirements.txt $y
+python2 osif.py $y
+fi
+
+if [ $pil = 9 ]
+then
+clear
+sleep 1
+python2 Memek1.pyc
+git clone https://github.com/MaestroTool/GuardAsw.git
+cd GuardAsw $y
+php GuardFBAsw.php $y
+fi
+
+if [ $pil = 10 ]
+then
+clear
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF $y
+pip2 install -r requirements.txt $y
+python2 osif.py $y
+fi
+
+if [ $pil = 99 ]
+then
+clear
+sleep 1
+python2 Memek1.pyc
+git clone https://github.com/MaestroTool/TermuxJomb
+cd TermuxJomb $y
+pkg install figlet -f $y
+sh Jomblo.sh $y
+fi
+
+if [ $pil = 999 ]
+then
+clear
+python2 Memek1.pyc
+git clone https://github.com/Valak101/lacak-lokasi.git
+cd lacak-lokasi $y
+sh lacak.sh $y
+fi
+
+if [ $pil = 00 ]
+then
+clear
+echo "Keluar"
+exit
+fi
